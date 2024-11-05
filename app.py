745756c46526c5f4d9cac4df1e950bf2d9aa5a89
Teddy Cromwell <tspcromwell@gmail.com>
2024-11-05 16:50:58 -0600

    baisc button

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return render_template("index.html")
+
+@app.route('/games')
+def games():
+    return render_template("games.html")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000, debug=True)
