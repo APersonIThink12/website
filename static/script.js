037883d46cf2cb82139f1bd21455db13f45f0c83
APersonIThink12 <157146445+APersonIThink12@users.noreply.github.com>
2024-11-06 15:06:48 +0000

    polished home page with new games btn and better home button with less lines of code.

--- static/script.js
+++ static/script.js
@@ -1,5 +1,10 @@
 const homeBtn = document.querySelector('.homeBtn button');
+const gamesBtn = document.querySelector('.gamesBtn button');
 
 homeBtn.addEventListener('click', () => {
   window.location.href = '/';
 });
+
+gamesBtn.addEventListener('click', () => {
+  window.location.href = '/games';
+})
