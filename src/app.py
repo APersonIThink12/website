a9fc39f01b13e9903e93652df1feecaeda1c812c
Teddy Cromwell <tspcromwell@gmail.com>
2025-09-24 20:39:59 -0500

    redoing stuff

--- src/app.py
+++ src/app.py
@@ -1,38 +1,14 @@
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
-
-@app.route('/games')
-def games():
-    return render_template("games.html")
-
-@app.route('/games/radiant-ruin')
-def radiantruin():
-    return render_template("radiant-ruin.html")
+    return render_template('index.html')
 
 @app.route('/contact')
 def contact():
-    return render_template("contact.html")
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/software')
-def software():
-    return render_template('software.html')
-
-@app.route('/software/ratchet')
-def ratchet():
-    return render_template('ratchet.html')
-
-@app.route('/downloadRatchet')
-def download_ratchet():
-    return send_file('static/ratchet_installer.exe', as_attachment=True)
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
